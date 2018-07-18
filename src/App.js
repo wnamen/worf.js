491f133d666e4db48d1d1136dde50d751dbc3ff3
wnamen <pmbnnigm8>
2018-07-18 16:05:37 -0700

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Navbar, NavItem, Button, Chip  } from 'react-materialize';
+import { Row, Button, Chip, Collection, CollectionItem  } from 'react-materialize';
 import ContentEditable from './ContentEditable.js';
 import moment from 'moment';
 import rp from 'request-promise';
@@ -51,14 +51,14 @@ class App extends Component {
             json: true
         }).then(res => {
             if (res.pass) {
-                this.setState({message: '', comments: [{name: 'Guest', date: moment(), message: this.state.message}, ...this.state.comments]})
+                this.setState({message: '', comments: [{name: 'Guest', date: moment(), message: (this.state.message).replace(/<(?:.|\n)*?>/gm, '')}, ...this.state.comments]})
             } else {
 
                 console.log(res);
 
                 if (res.problemIndices === -1) {
                     this.setState({
-                        message: `<span className="error">${this.state.message}</span>`,
+                        message: `<span class="error">${this.state.message}</span>`,
                         sentiment: 'Please review your message for errors you dumbass.'
                     })
 
@@ -66,7 +66,7 @@ class App extends Component {
                     let sentences = this.state.message.match(/([^.?!])*[.?!]?/g);
 
                     res.problemIndices.forEach((index) => {
-                        sentences[index] = `<span style={{backgroundColor: 'yellow'}}>${sentences[index]}</span>`
+                        sentences[index] = `<span class="red">${sentences[index]}</span>`
                     });
 
                     this.setState({
@@ -84,32 +84,30 @@ class App extends Component {
       console.log(this.state)
     return (
       <div className="App">
-        <Navbar>
-            <NavItem>600 Comments</NavItem>
-        </Navbar>
-
-        <div>
-            <ContentEditable placeholder="comment here" s={12} name="message" html={this.state.message} onChange={this.handleMessageInput} />
-            { this.state.sentiment }
-            <Row style={{textAlign: 'right'}}>
-              <Button onClick={() => this.setState({message: '', sentiment: ''})}>&#10007;</Button>
-              <Button onClick={this.handleSubmit} disabled={this.state.message.length <= 0}>&#10003;</Button>
-            </Row>
-        </div>
-
-        <Row>
-            {
-                this.state.comments.map((comment, idx)=>{
-                    return (
-                        <Row key={idx} s={12}>
-                            <Chip>{comment.name}</Chip>
-                            <small>{moment().calendar()}</small>
-                            <p>{comment.message}</p>
-                        </Row>
-                    )
-                })
-            }
-        </Row>
+        <Collection>
+            <CollectionItem>600 Comments</CollectionItem>
+            <CollectionItem>
+                <ContentEditable placeholder="comment here" s={12} name="message" html={this.state.message} onChange={this.handleMessageInput} />
+                { this.state.sentiment }
+                <Row style={{textAlign: 'right'}}>
+                    <Button className="red" onClick={() => this.setState({message: '', sentiment: ''})}>&#10007;</Button>
+                    <Button onClick={this.handleSubmit} disabled={this.state.message.length <= 0}>&#10003;</Button>
+                </Row>
+            </CollectionItem>
+            <CollectionItem>
+                {
+                    this.state.comments.map((comment, idx)=>{
+                        return (
+                            <Row key={idx} s={12}>
+                                <Chip>{comment.name}</Chip>
+                                <small>{moment().calendar()}</small>
+                                <p className="message">{comment.message}</p>
+                            </Row>
+                        )
+                    })
+                }
+            </CollectionItem>
+        </Collection>
       </div>
     );
   }
