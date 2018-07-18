518a7902aaf8561a6d1f636b6b1caba57a23890b
lweld1127 <lewhat@live.com>
2018-07-18 11:38:45 -0700

    initial comment frontend setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
 import React, { Component } from 'react';
+import { Row, Input, Navbar, NavItem, Button, Card  } from 'react-materialize';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            comments: [{
+                name: 'lewhat',
+                date: '12/32/45',
+                message: 'hello peoples'
+            },
+            {
+                name: 'lewhat',
+                date: '12/32/45',
+                message: 'hello peoples'
+            },
+            {
+                name: 'lewhat',
+                date: '12/32/45',
+                message: 'hello peoples'
+            },
+            {
+                name: 'lewhat',
+                date: '12/32/45',
+                message: 'hello peoples'
+            },
+            {
+                name: 'lewhat',
+                date: '12/32/45',
+                message: 'hello peoples'
+            }
+
+            ]
+        }
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+            <Navbar>
+               <NavItem>600 Comments</NavItem>
+            </Navbar>
+            <Row>
+              <Input placeholder="comment here" s={6} />
+            </Row>
+            <Row>
+              <Button>Cancel</Button>
+              <Button>Submit</Button>
+            </Row>
+            <Row>
+                {
+                    this.state.comments.map((comment, idx)=>{
+                        return (
+                            <Card key={idx}>
+                                <p>
+                                    <span>{comment.name}</span>
+                                    <span> {comment.date}</span>
+                                </p>
+                                    <p>{comment.message}</p>
+                            </Card>
+                        )
+                    })
+                }
+            </Row>
       </div>
     );
   }
