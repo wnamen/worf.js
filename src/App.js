80afe5326f90e664f5d18750ecd67e01dd088c01
lweld1127 <lewhat@live.com>
2018-07-18 17:39:59 -0700

    ui changes - reply

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Row, Button, Chip, Collection, CollectionItem  } from 'react-materialize';
+import { Row, Button, Chip, Collection, CollectionItem, Card  } from 'react-materialize';
 import ContentEditable from './ContentEditable.js';
+import FormInput from './FormInput.js';
 import moment from 'moment';
 import rp from 'request-promise';
 import './App.css';
@@ -13,27 +14,32 @@ class App extends Component {
             message: '',
             sentiment: '',
             comments: [{
-                name: 'lewhat',
+                name: 'Sam',
+                image: 'https://owl.cbsi.com/jira/secure/useravatar?ownerId=sdarb0326&avatarId=28101',
+                date: '12/32/45',
+                message: 'hello peoples'
+            },
+            {
+                name: 'Keerthi',
+                image: 'https://owl.cbsi.com/jira/secure/useravatar?ownerId=kbasireddy&avatarId=12915',
+                date: '12/32/45',
+                message: 'hello peoples'
+            },
+            {
+                name: 'Nick',
+                image: 'https://owl.cbsi.com/jira/secure/useravatar?avatarId=10122',
+                date: '12/32/45',
+                message: 'hello peoples'
+            },
+            {
+                name: 'William',
+                image: 'https://owl.cbsi.com/jira/secure/useravatar?avatarId=10102',
                 date: '12/32/45',
                 message: 'hello peoples'
             },
             {
                 name: 'lewhat',
-                date: '12/32/45',
-                message: 'hello peoples'
-            },
-            {
-                name: 'lewhat',
-                date: '12/32/45',
-                message: 'hello peoples'
-            },
-            {
-                name: 'lewhat',
-                date: '12/32/45',
-                message: 'hello peoples'
-            },
-            {
-                name: 'lewhat',
+                image: 'https://owl.cbsi.com/jira/secure/useravatar?ownerId=lweld1127&avatarId=23500',
                 date: '12/32/45',
                 message: 'hello peoples'
             }]
@@ -43,7 +49,9 @@ class App extends Component {
     handleMessageInput = (e) => {
         this.setState({message: e.target.value})
     };
-
+    handleClearMessage = () => {
+        this.setState({message: '', sentiment: ''});
+    }
     handleSubmit = () => {
         rp({
             method: 'get',
@@ -81,30 +89,42 @@ class App extends Component {
         });
     };
 
+    replySupport = () => {
+        this.setState({reply: true});
+    }
+
   render() {
       console.log(this.state)
     return (
       <div className="App">
         <Collection>
             <CollectionItem>
-                <ContentEditable className="input-box" placeholder="comment here" s={12} name="message" html={this.state.message} onChange={this.handleMessageInput} />
-                {
-                    this.state.sentiment && (<span className="error-message">{ this.state.sentiment }</span>)
-                }
-                <Row style={{textAlign: 'right'}}>
-                    <Button className="red" onClick={() => this.setState({message: '', sentiment: ''})}>&#10007;</Button>
-                    <Button onClick={this.handleSubmit} disabled={this.state.message.length <= 0}>&#10003;</Button>
-                </Row>
+                <FormInput message={this.state.message}
+                           handleMessageInput={this.handleMessageInput}
+                           sentiment={this.state.sentiment}
+                           handleClearMessage={this.handleClearMessage}
+                           handleSubmit={this.handleSubmit}/>
             </CollectionItem>
             <CollectionItem>
                 {
                     this.state.comments.map((comment, idx)=>{
                         return (
-                            <Row key={idx} s={12}>
-                                <Chip>{comment.name}</Chip>
+                            <Card key={idx} s={12}>
+                                <Chip>
+                                <img src={comment.image} width='20' alt="profile image"/>
+                                {comment.name}
+                                </Chip>
                                 <small>{moment().calendar()}</small>
                                 <p className="message">{comment.message}</p>
-                            </Row>
+                                <Button className="reply-button" onClick={this.replySupport}>Reply</Button>
+                                {
+                                    this.state.reply && (<FormInput message={this.state.message}
+                                                                   handleMessageInput={this.handleMessageInput}
+                                                                   sentiment={this.state.sentiment}
+                                                                   handleClearMessage={this.handleClearMessage}
+                                                                   handleSubmit={this.handleSubmit}/>)
+                                }
+                            </Card>
                         )
                     })
                 }
---
+++ src/FormInput.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Row, Button } from 'react-materialize';
+import ContentEditable from './ContentEditable.js';
+import './App.css';
+
+class FormInput extends Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+        return (
+            <div className="App">
+                <ContentEditable className="input-box" placeholder="comment here" s={12} name="message" html={this.props.message} onChange={this.props.handleMessageInput} />
+                {
+                    this.props.sentiment && (<span className="error-message">{ this.props.sentiment }</span>)
+                }
+                <Row style={{textAlign: 'right'}}>
+                    <Button className="red" onClick={this.props.handleClearMessage}>&#10007;</Button>
+                    <Button onClick={this.props.handleSubmit} disabled={this.props.message.length <= 0}>&#10003;</Button>
+                </Row>
+            </div>
+        );
+    }
+}
+
+export default FormInput;
